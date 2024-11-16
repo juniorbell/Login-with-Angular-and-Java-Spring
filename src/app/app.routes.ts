2c0872f84eea60880a59dea49bc34520220ba4e0
Donizete Junior <donizetejunioread@gmail.com>
2024-11-15 23:24:59 -0300

    page signup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { SignupComponet } from './pages/signup/signup.component';
 
 export const routes: Routes = [
     {
         path: "login", component: LoginComponent
+    },
+    {
+        path: "singup", component: SignupComponet
     }
 ];
